39bf0acb879027a768f4828e74b536350b0120f5
Suhani546 <123989536+Suhani546@users.noreply.github.com>
2023-07-20 14:39:03 +0530

    Add files via upload

---
+++ Netflix clone/script.js
@@ -0,0 +1,95 @@
+let ans1 = document.getElementById("ans1");
+let quesAns_box1 = document.getElementById("quesAns_box1")
+let icon1 = document.getElementById("icon1")
+
+quesAns_box1.addEventListener('click', drop1)
+function drop1(){
+    if(ans1.style.display === "block"){
+        ans1.style.display = 'none'
+        icon1.style.transform = null
+    }
+    else{
+        ans1.style.display = "block"
+        icon1.style.transform = 'rotate(45deg)'
+    }
+}
+
+let ans2 = document.getElementById("ans2");
+let quesAns_box2 = document.getElementById("quesAns_box2")
+let icon2 = document.getElementById("icon2")
+
+quesAns_box2.addEventListener('click', drop2)
+function drop2(){
+    if(ans2.style.display === "block"){
+        ans2.style.display = 'none'
+        icon2.style.transform = null
+    }
+    else{
+        ans2.style.display = "block"
+        icon2.style.transform = 'rotate(45deg)'
+    }
+}
+
+let ans3 = document.getElementById("ans3");
+let quesAns_box3 = document.getElementById("quesAns_box3")
+let icon3 = document.getElementById("icon3")
+
+quesAns_box3.addEventListener('click', drop3)
+function drop3(){
+    if(ans3.style.display === "block"){
+        ans3.style.display = 'none'
+        icon3.style.transform = null
+    }
+    else{
+        ans3.style.display = "block"
+        icon3.style.transform = 'rotate(45deg)'
+    }
+}
+
+let ans4 = document.getElementById("ans4");
+let quesAns_box4 = document.getElementById("quesAns_box4")
+let icon4 = document.getElementById("icon4")
+
+quesAns_box4.addEventListener('click', drop4)
+function drop4(){
+    if(ans4.style.display === "block"){
+        ans4.style.display = 'none'
+        icon4.style.transform = null
+    }
+    else{
+        ans4.style.display = "block"
+        icon4.style.transform = 'rotate(45deg)'
+    }
+}
+
+let ans5 = document.getElementById("ans5");
+let quesAns_box5 = document.getElementById("quesAns_box5")
+let icon5 = document.getElementById("icon5")
+
+quesAns_box5.addEventListener('click', drop5)
+function drop5(){
+    if(ans5.style.display === "block"){
+        ans5.style.display = 'none'
+        icon5.style.transform = null
+    }
+    else{
+        ans5.style.display = "block"
+        icon5.style.transform = 'rotate(45deg)'
+    }
+}
+
+let ans6 = document.getElementById("ans6");
+let quesAns_box6 = document.getElementById("quesAns_box6")
+let icon6 = document.getElementById("icon6")
+
+quesAns_box6.addEventListener('click', drop6)
+function drop6(){
+    if(ans6.style.display === "block"){
+        ans6.style.display = 'none'
+        icon6.style.transform = null
+    }
+    else{
+        ans6.style.display = "block"
+        icon6.style.transform = 'rotate(45deg)'
+    }
+}
